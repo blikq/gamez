5317ae0fd97cce51b464eab4ba0adc0895c744f0
Blikq <blikqmain@gmail.com>
2022-11-10 12:21:51 +0100

    all_working

--- converge/models.py
+++ converge/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from pymongo import MongoClient
-
+import json
 # Create your models here.
 
 key = MongoClient()
@@ -14,18 +14,35 @@ steamunlocked_doc = collection["steamunlocked"]
 def create_and_update_names():
     names_1 = fitgirl_doc.find()
     names_2 = steamunlocked_doc.find()
-    name = []
-    for cont1 in names_1:
-        pop = converge_doc.find({"name": cont1["cont1"]})
-        if pop != None:
-            pass
-        else:
-            if pop["name"].lower() == cont1["name"].lower():
-                pass
+    for cont in names_1:
+        converge_doc.update_one({"name": cont["name"]}, {"$set": {"name": cont["name"], "content": []}}, upsert=True)
+    for cont in names_2:
+        converge_doc.update_one({"name": cont["name"]}, {"$set": {"name": cont["name"],  "content": []}}, upsert=True)
 
 def create_and_update_name():
     check_if_exist = converge_doc.estimated_document_count()
     if check_if_exist == 0:
         create_and_update_names()
-    
-print(create_and_update_name())
+
+def update_content():
+    ind = converge_doc.find()
+    for i in ind:
+        #DECIDED TO USE AHASJMAP METHOD
+        # cont1 = fitgirl_doc.find_one({"name": i["name"]})
+        # if cont1 is not None:
+        #     converge_doc.update_one({"name": cont1["name"]}, {"$push": {"content": cont1}})
+
+        # cont2 = steamunlocked_doc.find_one({"name": i["name"]})
+        # if cont2 is not None:
+        #     converge_doc.update_one({"name": cont2["name"]}, {"$push": {"content": cont2}})
+        cont1 = fitgirl_doc.find_one({"name": i["name"]})
+        if cont1 is not None:
+            converge_doc.update_one({"name": cont1["name"]}, {"$set": {"fitgirl": cont1}})
+            ###
+        cont2 = steamunlocked_doc.find_one({"name": i["name"]})
+        if cont2 is not None:
+            converge_doc.update_one({"name": cont2["name"]}, {"$set": {"steamunlocked": cont2}})
+
+
+create_and_update_names()
+update_content()
